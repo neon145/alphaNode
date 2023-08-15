bf421b36606c17e2ec3478dd6009de559d613c89
neon145 <aj.sendtext@gmail.com>
2023-08-15 20:39:23 +0530

    Created Server

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const PORT = 8080;
 //add the JSON middleware
 app.use(express.json());
 
+//instanciate the express server 
+app.listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}`);
+});
+
 //setup a get handler
 app.get('/hello', function(req, res) {
     res.status(200).send({
